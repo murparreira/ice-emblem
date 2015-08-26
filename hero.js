8d6db85855bb1a1999171cfaa90ed586c18d5d58
Murillo Parreira <murillo.parreira@agrodefesa.go.gov.br>
2015-08-26 17:11:31 -0300

    Added Stage and Map

--- hero.js
+++ hero.js
@@ -1,3 +1,13 @@
+function Map(options) {
+  var defaultOptions = {
+    width: 10,
+    height: 10
+  },
+  options = $.extend(defaultOptions, options);
+  this.width = options.width;
+  this.height = options.height;
+}
+
 function Weapon(options) {
   var defaultOptions = {
     weaponType: 'physical', 
@@ -83,43 +93,14 @@ Hero.prototype = {
   }
 }
 
-var chrom = new Hero({
-  nickname: 'Chrom',
-  weapons: [
-    new Weapon({weaponName: 'Falchion', weaponType: 'physical', weaponBaseDamage: 4, weaponBaseAccuracy: 100}),
-    new Weapon({weaponName: 'Steel Sword', weaponType: 'physical', weaponBaseDamage: 5, weaponBaseAccuracy: 90})
-  ],
-  hp: 80,
-  strength: 2,
-  agility: 4,
-  magic: 1,
-  defense: 5,
-  resistance: 2
-})
-
-var sumia = new Hero({
-  nickname: 'Sumia',
-  weapons: [
-    new Weapon({weaponName: 'Lance of Destiny', weaponType: 'magical', weaponBaseDamage: 7, weaponBaseAccuracy: 85}),
-    new Weapon({weaponName: 'Blood Lance', weaponType: 'physical', weaponBaseDamage: 5, weaponBaseAccuracy: 80})
-  ],
-  hp: 90,
-  strength: 1,
-  agility: 3,
-  magic: 5,
-  defense: 1,
-  resistance: 4
-})
-
-var tactician = new Hero({
-  nickname: 'Tactician',
-  weapons: [
-    new Weapon({weaponName: 'Tome of Destruction', weaponType: 'magical', weaponBaseDamage: 10, weaponBaseAccuracy: 75})
-  ],
-  hp: 85,
-  strength: 1,
-  agility: 2,
-  magic: 7,
-  defense: 3,
-  resistance: 3
-})
+function Stage(options) {
+  var defaultOptions = {
+    map: new Map(),
+    heroes: [new Hero()]
+    enemies: [new Hero()]
+  },
+  options = $.extend(defaultOptions, options);
+  this.map = options.map;
+  this.heroes = options.heroes;
+  this.enemies = options.enemies;
+}
---
+++ test.js
@@ -0,0 +1,40 @@
+var chrom = new Hero({
+  nickname: 'Chrom',
+  weapons: [
+    new Weapon({weaponName: 'Falchion', weaponType: 'physical', weaponBaseDamage: 4, weaponBaseAccuracy: 100}),
+    new Weapon({weaponName: 'Steel Sword', weaponType: 'physical', weaponBaseDamage: 5, weaponBaseAccuracy: 90})
+  ],
+  hp: 80,
+  strength: 2,
+  agility: 4,
+  magic: 1,
+  defense: 5,
+  resistance: 2
+});
+
+var sumia = new Hero({
+  nickname: 'Sumia',
+  weapons: [
+    new Weapon({weaponName: 'Lance of Destiny', weaponType: 'magical', weaponBaseDamage: 7, weaponBaseAccuracy: 85}),
+    new Weapon({weaponName: 'Blood Lance', weaponType: 'physical', weaponBaseDamage: 5, weaponBaseAccuracy: 80})
+  ],
+  hp: 90,
+  strength: 1,
+  agility: 3,
+  magic: 5,
+  defense: 1,
+  resistance: 4
+});
+
+var tactician = new Hero({
+  nickname: 'Tactician',
+  weapons: [
+    new Weapon({weaponName: 'Tome of Destruction', weaponType: 'magical', weaponBaseDamage: 10, weaponBaseAccuracy: 75})
+  ],
+  hp: 85,
+  strength: 1,
+  agility: 2,
+  magic: 7,
+  defense: 3,
+  resistance: 3
+});
